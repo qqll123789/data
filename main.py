9475cd270c6d177a72db1dff0a384adf610ff75b
ql <798864699@qq.com>
2024-03-07 16:58:58 +0800

    修改main.py

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ import unittestreport
 class TestBaidu(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.driver = webdriver.get('https://www.baidu.com')
+        self.driver = webdriver.Chrome()
 
     def test_01(self):
         '''执行用例1'''
+        self.driver.get('https://www.baidu.com')
         print('001')
 
     def test_01(self):
         '''执行用例2'''
+        self.driver.get('https://www.baidu.com')
         print('002')
 
     def tearDown(self) -> None:
         self.driver.close()
 
-if __name__ == '__main':
+if __name__ == '__main__':
     unittest.main()
